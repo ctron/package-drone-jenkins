e741a586c5c0de5891e55ecf23f7e368ce775d47
Nikolas Falco <nfalco79@hotmail.com>
2017-06-04 22:34:56 +0200

    Add support for Jenkins DSL pipeline. Fix some issues in check validation. Add support for i18n for messages in the main class. Add some missing javadoc and unit test

---
+++ package-drone/src/test/java/de/dentrassi/pm/jenkins/DroneRecorderValidatorTest.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Nikolas Falco.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nikolas Falco - author of some PRs
+ *******************************************************************************/
+package de.dentrassi.pm.jenkins;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import de.dentrassi.pm.jenkins.DroneRecorder.DescriptorImpl;
+import hudson.FilePath;
+import hudson.model.FreeStyleProject;
+import hudson.util.FormValidation;
+import hudson.util.FormValidation.Kind;
+
+/**
+ * Test input form validation.
+ *
+ * @author Nikolas Falco
+ */
+@RunWith ( PowerMockRunner.class )
+@PrepareForTest ( { FreeStyleProject.class, FilePath.class } )
+public class DroneRecorderValidatorTest
+{
+
+    @Test
+    public void test_artifacts_validation_when_on_pipeline_sysntax () throws Exception
+    {
+        DescriptorImpl descriptor = new DescriptorImpl ();
+
+        FormValidation result = descriptor.doCheckArtifacts ( null, "*.jar" );
+        assertThat ( result.kind, is ( Kind.OK ) );
+    }
+
+    @Test
+    public void test_artifacts_validation () throws Exception
+    {
+        FreeStyleProject prj = mock ( FreeStyleProject.class );
+        FilePath wks = mock ( FilePath.class );
+        FormValidation expectedValidation = FormValidation.error ( "test" );
+        when ( wks.validateFileMask ( Mockito.anyString () ) ).thenReturn ( expectedValidation );
+        doReturn ( wks ).when ( prj ).getSomeWorkspace ();
+
+        DescriptorImpl descriptor = new DescriptorImpl ();
+        FormValidation result = descriptor.doCheckArtifacts ( prj, "*.jar" );
+        assertThat ( result.kind, is ( expectedValidation.kind ) );
+        assertThat ( result.getMessage (), is ( expectedValidation.getMessage () ) );
+    }
+
+    @Test
+    public void test_empty_server_url () throws Exception
+    {
+        DescriptorImpl descriptor = new DescriptorImpl ();
+
+        FormValidation result = descriptor.doCheckServerUrl ( "" );
+        assertThat ( result.kind, is ( Kind.WARNING ) );
+        assertThat ( result.getMessage (), is ( Messages.DroneRecorder_DescriptorImpl_emptyServerUrl () ) );
+    }
+
+    @Test
+    public void test_server_url_that_contains_variable () throws Exception
+    {
+        DescriptorImpl descriptor = new DescriptorImpl ();
+
+        FormValidation result = descriptor.doCheckServerUrl ( "${PDRONE_URL}/root" );
+        assertThat ( result.kind, is ( Kind.OK ) );
+        result = descriptor.doCheckServerUrl ( "http://${SERVER_NAME}/root" );
+        assertThat ( result.kind, is ( Kind.OK ) );
+        result = descriptor.doCheckServerUrl ( "http://acme.com/${CONTEXT_ROOT}" );
+        assertThat ( result.kind, is ( Kind.OK ) );
+    }
+
+    @Test
+    public void test_server_url_invalid_protocol () throws Exception
+    {
+        DescriptorImpl descriptor = new DescriptorImpl ();
+
+        FormValidation result = descriptor.doCheckServerUrl ( "hpp://acme.com/root" );
+        assertThat ( result.kind, is ( Kind.ERROR ) );
+        assertThat ( result.getMessage (), startsWith ( Messages.DroneRecorder_DescriptorImpl_invalidServerUrl ( "" ) ) );
+    }
+
+}
